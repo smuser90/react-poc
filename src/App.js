13b2ffd62c435f8224554ce362aedb8a1b855a1b
smuser90 <sam.musso90@gmail.com>
2018-09-29 15:53:59 -0600

    Renders load and target, can handle 500 axes updating 150 per frame at 50fps

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 import './App.css'
 import Axis from './Axis'
 
-const AXIS_COUNT = 100;
+/*
+  100 Axes -> 60fps
+  150 Axes -> 60fps
+  200 Axes -> 50fps
+  250 Axes -> 40fps
+*/
+
+const AXIS_COUNT = 500;
 
 var AXIS_LIMIT = 1000;
 
@@ -16,6 +23,7 @@ class App extends Component {
     this.axes = [];
     this.axisState = 'ready';
     this.axisPosition = 0;
+    this.quarter = 0;
 
     for(var axis = 0; axis < AXIS_COUNT; axis++)
       this.axes.push(<Axis index={axis} position={this.axisPosition} axisState={this.axisState} key={Math.random()}/>)
@@ -25,30 +33,52 @@ class App extends Component {
     }.bind(this), 1000);
   }
 
+  updateQuarter(){
+    this.quarter = this.quarter+1;
+    if(this.quarter == 4) this.quarter = 0;
+    console.log("Quarter is: "+this.quarter);
+  }
+
   updateAxesState(){
     // schedule the next frame
     setTimeout(function() { this.updateAxesState(); }.bind(this), 16);
 
-    if(this.axisPosition === 0)
+    var fifth = AXIS_LIMIT / 5;
+    if(this.axisPosition ===  0){
       this.axisState = "ready"
+      this.updateQuarter();
+    }
 
-    if(this.axisPosition === 200)
+    if(this.axisPosition === fifth){
       this.axisState = "moving"
+      this.updateQuarter();
+    }
 
-    if(this.axisPosition === 400)
+    if(this.axisPosition === 2*fifth){
       this.axisState = "warning"
-
-    if(this.axisPosition === 600)
+      this.updateQuarter();
+    }
+    if(this.axisPosition === 3*fifth){
       this.axisState = "estop"
+      this.updateQuarter();
+    }
 
-    if(this.axisPosition === 800)
+    if(this.axisPosition === 4*fifth){
       this.axisState = "disabled"
+      this.updateQuarter();
+    }
+
+    var quanta = (AXIS_COUNT / 4);
+    var min = this.quarter * quanta;
+    var max = min + quanta;
 
     for(var axis in this.axes){
-      this.axes[axis] = React.cloneElement(this.axes[axis], {
-        position: this.axisPosition,
-        axisState: this.axisState,
-      })
+      if(axis >= min && axis <= max){
+        this.axes[axis] = React.cloneElement(this.axes[axis], {
+          position: this.axisPosition,
+          axisState: this.axisState,
+        })
+      }
     }
 
     this.axisPosition++;
--- src/Axis.js
+++ src/Axis.js
@@ -26,9 +26,11 @@ class Axis extends Component {
       <div className="Axis" state={this.props.axisState}>
       <div className="Grid">
       <img src={gear} className="AxisIcon" alt="gear" />
-        <div className="Position">{this.props.position}.00 ft</div>
-        <h3 className="AxisTitle">{this.name}</h3>
         <div className="stylie"><div className="dot" style={dotStyle}/></div>
+        <h3 className="AxisTitle">{this.name}</h3>
+        <div className="Load">{parseInt(this.props.position / 10 % 7)} lbs</div>
+        <div className="Position">{this.props.position}.00 ft</div>
+        <div className="Target">1000.00 ft</div>
 
       </div>
 
